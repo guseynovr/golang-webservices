f0e8264b269ccacfb3b83de5de55d46544b48c64
Ruslan Guseynov <rslnguseynov@gmail.com>
2022-03-01 17:26:12 +0300

    hw3 continued

--- hw3_bench/fast.go
+++ hw3_bench/fast.go
@@ -2,9 +2,120 @@ package main
 
 import (
 	"io"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strings"
 )
+
+// const filePath string = "./data/users.txt"
+
+// func main() {
+// 	FastSearch(os.Stdout)
+// }
 
 // вам надо написать более быструю оптимальную этой функции
 func FastSearch(out io.Writer) {
-	SlowSearch(out)
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+
+	fileContents, err := ioutil.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+
+	r := regexp.MustCompile("@")
+	// seenBrowsers := []string{}
+	seenBrowsers := make(map[string]bool)
+	uniqueBrowsers := 0
+	foundUsers := ""
+
+	lines := strings.Split(string(fileContents), "\n")
+
+	users := make([]map[string]interface{}, 0)
+	for _, line := range lines {
+		user := make(map[string]interface{})
+		// fmt.Printf("%v %v\n", err, line)
+		err := json.Unmarshal([]byte(line), &user)
+		if err != nil {
+			panic(err)
+		}
+		users = append(users, user)
+	}
+
+	for i, user := range users {
+
+		isAndroid := false
+		isMSIE := false
+
+		browsers, ok := user["browsers"].([]interface{})
+		if !ok {
+			// log.Println("cant cast browsers")
+			continue
+		}
+
+		for _, browserRaw := range browsers {
+			browser, ok := browserRaw.(string)
+			if !ok {
+				// log.Println("cant cast browser to string")
+				continue
+			}
+			r1 := regexp.MustCompile("Android")
+			if ok := r1.MatchString(browser); ok {
+				isAndroid = true
+				// notSeenBefore := true
+				if _, exists := seenBrowsers[browser]; !exists {
+					seenBrowsers[browser] = true
+					// seenBrowsers = append(seenBrowsers, browser)
+					uniqueBrowsers++
+				}
+				// for _, item := range seenBrowsers {
+				// 	if item == browser {
+				// 		notSeenBefore = false
+				// 	}
+				// }
+				// if notSeenBefore {
+				// 	// log.Printf("SLOW New browser: %s, first seen: %s", browser, user["name"])
+				// 	seenBrowsers = append(seenBrowsers, browser)
+				// 	uniqueBrowsers++
+				// }
+			}
+
+			r2 := regexp.MustCompile("MSIE")
+			if ok := r2.MatchString(browser); ok {
+				isMSIE = true
+				// notSeenBefore := true
+				if _, exists := seenBrowsers[browser]; !exists {
+					seenBrowsers[browser] = true
+					// seenBrowsers = append(seenBrowsers, browser)
+					uniqueBrowsers++
+				}
+				// for _, item := range seenBrowsers {
+				// 	if item == browser {
+				// 		notSeenBefore = false
+				// 	}
+				// }
+				// if notSeenBefore {
+				// 	// log.Printf("SLOW New browser: %s, first seen: %s", browser, user["name"])
+				// 	seenBrowsers = append(seenBrowsers, browser)
+				// 	uniqueBrowsers++
+				// }
+			}
+		}
+
+		if !(isAndroid && isMSIE) {
+			continue
+		}
+
+		// log.Println("Android and MSIE user:", user["name"], user["email"])
+		email := r.ReplaceAllString(user["email"].(string), " [at] ")
+		foundUsers += fmt.Sprintf("[%d] %s <%s>\n", i, user["name"], email)
+	}
+
+	fmt.Fprintln(out, "found users:\n"+foundUsers)
+	fmt.Fprintln(out, "Total unique browsers", len(seenBrowsers))
 }
---
+++ hw3_bench/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -v //Users/dgidget/Documents/go/src/coursera/coursera_hw/hw3_bench:/go -it --rm --name mailgo mailgo
